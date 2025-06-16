9fd66796adc541140e2a6d759fa134ff58ef1f93
dpolygon <dannyg@lonestardev.net>
2025-06-15 21:16:21 -0500

    background now white

--- src/components/visualTextPair.jsx
+++ src/components/visualTextPair.jsx
@@ -5,7 +5,7 @@ export default function VisualTextPair({ imageUrl, text }) {
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
             <img src={imageUrl} style={{ height: '160px', width: '160px', borderRadius: '50%'}}></img>
             <div style={{padding: '1rem 0 0rem 0rem'}}>
-                <h4 style={{color: 'black', transform: 'scaleY(1.1)', fontWeight: '500'}}>{text}</h4>
+                <h4 style={{color: 'black', fontWeight: '500'}}>{text}</h4>
             </div>
         </div>
     )
