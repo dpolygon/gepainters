a4d2f5e343788396bdb9dd0ac4628c879642b9ac
dpolygon <dannyg@lonestardev.net>
2025-07-17 19:01:50 -0500

    updated images

--- src/components/contactUsForm.jsx
+++ src/components/contactUsForm.jsx
@@ -5,121 +5,94 @@ import './contactUsForm.css'
 // import { config } from '../../Constants'
 
 export default function ContactUsForm() {
-    const [activeButton, setActiveButton] = useState('text');
     const [isLoading, setIsLoading] = useState(false);
-    const [text, setText] = useState({
+    const [message, setMessage] = useState({
         name: "",
-        contactInfo: "",
-        message: "",
+        phoneNumber: "",
+        address: "",
+        memo: "",
     });
 
     const handleStateChange = (event) => {
         const { name, value } = event.target;
-        setText({
-            ...text,
+        setMessage({
+            ...message,
             [name]: value
         });
     }
 
-    // const sendMail = () => {
-    //     fetch(`${config.url.BACKEND_URL}/api/send-mail`, {
-    //         method: 'POST',
-    //         headers: { "Content-Type": "application/json" },
-    //         body: JSON.stringify(text)
-    //     })
-    //         .then(() => {
-    //             setText({
-    //                 name: "",
-    //                 contactInfo: "",
-    //                 message: "",
-    //             })
-    //         })
-    //         .finally(() => {
-    //             setIsLoading(false);
-    //         });
-    // }
+    const sendMessage = (event) => {
+        event.preventDefault();
+        setIsLoading(true);
 
-    // const sendText = () => {
-    //     fetch(`${config.url.BACKEND_URL}/api/send-text`, {
-    //         method: 'POST',
-    //         headers: { "Content-Type": "application/json" },
-    //         body: JSON.stringify(text)
-    //     })
-    //         .then(() => {
-    //             setText({
-    //                 name: "",
-    //                 contactInfo: "",
-    //                 message: "",
-    //             })
-    //         })
-    //         .finally(() => {
-    //             setIsLoading(false);
-    //         });
-    // }
+        fetch(`https://enqueue-message-task-r2nu66eilq-uc.a.run.app`, {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(message)
+        })
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error("Failed to enqueue task");
+                }
+                setMessage({
+                    name: "",
+                    phoneNumber: "",
+                    address: "",
+                    memo: "",
+                });
+            })
+            .catch((err) => {
+                alert("Something went wrong sending your message.");
+                console.error("Send failed:", err);
+            })
+            .finally(() => {
+                alert("Your message was delivered, thank you!")
+                setIsLoading(false);
+            });
+    }
 
-    // const handleEmailClick = async () => {
-    //     if (text.name === "" || text.message === "" || text.contactInfo === "") {
-    //         return
-    //     }
-    //     setIsLoading(true);
-    //     if (activeButton === "text") {
-    //         sendText();
-    //     } else {
-    //         sendMail();
-    //     }
-    // }
 
     return (
         <div className='ContactUsForm'>
-            {
-                // uncomment if you
-            /* <div style={{ display: 'flex', flexDirection: 'column', paddingBottom: '1rem', justifyContent: 'left' }}>
-                <h2>Communication Preference:</h2>
-                <div>
-                    <button className='contactUsPreferenceButton' onClick={() => setActiveButton('text')} style={{ backgroundColor: activeButton === "text" ? "lightgrey" : "black", border: '1px white solid' }}>
-                        Text
-                    </button>
-                    <button className='contactUsPreferenceButton' onClick={() => setActiveButton('email')} style={{ backgroundColor: activeButton === "email" ? "lightgrey" : "black", border: '1px white solid' }}>
-                        Email
-                    </button>
-                </div>
-            </div> */}
-            <form style={{ display: 'flex', flexDirection: 'column', gap: '1rem'}}>
-                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', gap: '1rem'}}>
+            <form onSubmit={sendMessage} style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', gap: '1rem' }}>
                     <input required
-                        style={{padding: '2rem', width: '100%'}}
+                        style={{ padding: '2rem', width: '100%' }}
                         className='ContactUsInput'
-                        value={text.name}
+                        value={message.name}
                         onChange={handleStateChange}
                         name="name"
                         placeholder="Name"
                     />
                     <input required
-                        style={{padding: '2rem', width: '100%'}}
+                        style={{ padding: '2rem', width: '100%' }}
                         className='ContactUsInput'
-                        value={text.contactInfo}
+                        value={message.phoneNumber}
                         onChange={handleStateChange}
-                        name="contactInfo"
-                        placeholder={activeButton === "text" ? "Mobile Number" : "E-mail"}
+                        name="phoneNumber"
+                        placeholder={"Phone Number"}
                     />
                 </div>
                 <input required
                     className='ContactUsInput'
-                    placeholder='Workplace Address'
-                    />
+                    value={message.address}
+                    onChange={handleStateChange}
+                    name='address'
+                    placeholder={'Workplace Address'}
+                />
                 <textarea required
                     style={{
                         resize: 'none',
                         padding: '2rem 2rem 6rem 2rem',
                     }}
                     className='ContactUsInput'
-                    value={text.message}
+                    value={message.memo}
                     onChange={handleStateChange}
-                    name='message'
+                    name='memo'
                     placeholder='Ask us anything or give us a brief description of your project 🏠🏬'
                     maxLength="4000"
                 />
-                <button style={{ height: '80px', fontSize: '15px' }} className='ContactUsSendButton' disabled={isLoading}>
+                <button type='submit' style={{ height: '80px', fontSize: '15px' }} className='ContactUsSendButton' disabled={isLoading}>
                     {isLoading ? 'Sending...' : 'Send'}
                 </button>
             </form>
--- src/components/servicecell.jsx
+++ src/components/servicecell.jsx
@@ -5,14 +5,14 @@ import './serviceCell.css';
 const ServiceCell = ({ title, subtitle, desc, src, icon: Icon, link }) => {
   return (
     <div className="service-cell">
-      <img className="service-image" src={src} alt={title} />
+      <video  loop autoPlay muted playsInline className="service-image" src={src} alt={title} />
       <div className='service-content'>
         <div style={{background: 'rgb(256, 256, 256, .7)', paddingTop:' 1rem', boxShadow: '0 0 80px 70px rgb(256, 256, 256, .75)'}}>
           <div style={{height: '180px'}}>
             <h1 className="service-title">{title}</h1>
             <h3 className="service-desc">{desc}</h3>
           </div>
-            <div className="service-footer">
+          <div className="service-footer">
             <div className="service-subtitle-wrapper">
               {Icon && <Icon fill='black' className="service-icon" />}
               <h3 className="service-subtitle">{subtitle}</h3>
